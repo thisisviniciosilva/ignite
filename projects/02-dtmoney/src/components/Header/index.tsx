b3a44b34645d76def1befaedcc558e45031cb2a8
Vinicio Silva <viniciomsilva@outlook.com.br>
2021-05-05 20:36:43 -0300

    ci(dtmoney/app): configures new transaction registration modal

--- projects/02-dtmoney/src/components/Header/index.tsx
+++ projects/02-dtmoney/src/components/Header/index.tsx
@@ -2,12 +2,16 @@ import logoImg from "../../assets/logo.svg";
 
 import { Container, Content } from "./styles";
 
-export function Header() {
+interface HeaderProps {
+  onOpenNewTransactionModal: () => void;
+}
+
+export function Header({ onOpenNewTransactionModal }: HeaderProps) {
   return (
     <Container>
       <Content>
         <img src={logoImg} alt="dt money" />
-        <button>Transação</button>
+        <button onClick={onOpenNewTransactionModal}>Nova Transação</button>
       </Content>
     </Container>
   );
