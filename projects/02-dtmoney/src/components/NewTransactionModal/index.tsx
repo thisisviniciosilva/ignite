c57af977bb72a3f209a995b4a909600bf2398020
Vinicio Silva <viniciomsilva@outlook.com.br>
2021-05-05 22:17:09 -0300

    style(dtmoney): structures and stylizes the new transaction registration modal

--- projects/02-dtmoney/src/components/NewTransactionModal/index.tsx
+++ projects/02-dtmoney/src/components/NewTransactionModal/index.tsx
@@ -1,5 +1,9 @@
 import Modal from "react-modal";
 
+import closeImg from "../../assets/close.svg";
+
+import { Container } from "./styles";
+
 interface NewTransactionModalProps {
   isOpen: boolean;
   onRequestClose: () => void;
@@ -10,8 +14,31 @@ export function NewTransactionModal({
   onRequestClose,
 }: NewTransactionModalProps) {
   return (
-    <Modal isOpen={isOpen} onRequestClose={onRequestClose}>
-      <h2>Cadastrar transação</h2>
+    <Modal
+      isOpen={isOpen}
+      onRequestClose={onRequestClose}
+      overlayClassName="react-modal-overlay"
+      className="react-modal-content"
+    >
+      <button
+        className="react-modal-close"
+        type="button"
+        onClick={onRequestClose}
+      >
+        <img src={closeImg} alt="Fechar" />
+      </button>
+
+      <Container>
+        <h2>Cadastrar transação</h2>
+
+        <input placeholder="Título" />
+
+        <input type="number" placeholder="Valor" />
+
+        <input placeholder="Categoria" />
+
+        <button type="submit">Cadastrar</button>
+      </Container>
     </Modal>
   );
 }
