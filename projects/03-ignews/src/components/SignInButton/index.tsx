1d1eff36864eae4f5d9b1641457dc3e89cab515f
Vinicio Silva <viniciomsilva@outlook.com.br>
2021-05-23 19:14:56 -0300

    feat(ignews): authenticates the app with github

--- projects/03-ignews/src/components/SignInButton/index.tsx
+++ projects/03-ignews/src/components/SignInButton/index.tsx
@@ -1,19 +1,28 @@
+import { signIn, signOut, useSession } from "next-auth/client";
 import { FaGithub } from "react-icons/fa";
 import { FiX } from "react-icons/fi";
 
 import styles from "./styles.module.scss";
 
 export function SignInButton() {
-  const isUserLoggedIn = true;
+  const [session] = useSession();
 
-  return isUserLoggedIn ? (
+  return session ? (
     <button className={styles.signInButton} type="button">
       <FaGithub color="#04d361" />
-      Vinicio Silva
-      <FiX className={styles.closeIcon} color="#737380" />
+      {session.user.name}
+      <FiX
+        className={styles.closeIcon}
+        color="#737380"
+        onClick={() => signOut()}
+      />
     </button>
   ) : (
-    <button className={styles.signInButton} type="button">
+    <button
+      className={styles.signInButton}
+      type="button"
+      onClick={() => signIn("github")}
+    >
       <FaGithub color="#eba417" />
       Sign in with GitHub
     </button>
